ab36e139ac9130c3c3113f1b7ef53d80eac289c2
yehians <yehians@ac.sce.ac.il>
2023-04-25 18:21:32 +0300

    changes

--- FlyBetter/settings.py
+++ FlyBetter/settings.py
@@ -77,21 +77,20 @@ WSGI_APPLICATION = 'FlyBetter.wsgi.application'
 
 DATABASES = {
 
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'flybetter_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+    #'default': {
+    #   'NAME': 'flybetter_db',
+     #   'USER': 'root',
+      #  'PASSWORD': '',
+     #   'HOST': 'localhost',
+     #   'PORT': '3306',
+    #}
 
 
     
-    #'default': {
-     #   'ENGINE': 'django.db.backends.sqlite3',
-      #  'NAME': BASE_DIR / 'db.sqlite3',
-    #}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
     
 }
 
