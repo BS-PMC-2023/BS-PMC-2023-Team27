d3953bce6dc3b0678ced4c9bb3c8304bf9fe7f65
yehians <yehians@ac.sce.ac.il>
2023-04-25 10:11:29 +0300

    BSPMC2327-19 create view to add. worker back end

--- Boards/views.py
+++ Boards/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,HttpResponse,redirect
+
+from . import forms
 from .models import USER
 from django.contrib import messages
+from django.contrib.auth.models import Group
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def Home(request):
@@ -43,3 +47,25 @@ def LogIN(request):
 
     print("aaaaaaaa")
     return render(request,'LogIn.html')
+
+
+def worker_signup(request):
+    userForm = forms.WorkerUserForm()
+    workerForm = forms.WorkerForm()
+
+    mydict = {'userForm': userForm, 'workerForm': workerForm}
+    if request.method == 'POST':
+        userForm = forms.WorkerUserForm(request.POST)
+        workerForm = forms.WorkerForm(request.POST, request.FILES)
+        print(workerForm.is_valid())
+        if userForm.is_valid() and workerForm.is_valid():
+            user = userForm.save()
+            user.set_password(user.password)
+            user.save()
+            worker = workerForm.save(commit=False)
+            worker.user = user
+            worker.save()
+            my_customer_group = Group.objects.get_or_create(name='WORKER')
+            my_customer_group[0].user_set.add(user)
+        return HttpResponseRedirect('LogIn')
+    return render(request, 'workersignup.html', context=mydict)
