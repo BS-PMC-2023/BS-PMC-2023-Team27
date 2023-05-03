ac236e23e8f3af66dd82ba35328de23c903571e9
yehians <yehians@ac.sce.ac.il>
2023-05-03 13:08:30 +0300

    Unit test

--- Boards/tests.py
+++ Boards/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+import  unittest
+from django.test import tag
+from django.urls import resolve,reverse
+from Boards import views
 
-# Create your tests here.
+@tag("unit_test")
+class TestUrls(SimpleTestCase):
+    def Test_HomePageadmin_url_resolves(self):
+        url=reverse('HomePageadmin')
+        self.assrtEquals(resolve(url).func,HomePageadmin)
+
