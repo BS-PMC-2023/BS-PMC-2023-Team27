f8858c4348780bf620f35cfd46b97cc086c9f370
yehians <yehians@ac.sce.ac.il>
2023-06-11 12:16:14 +0300

    end

--- Boards/forms.py
+++ Boards/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 from django.contrib.auth.models import User
 
-from models import Passenger, Worker, ContactUs, Workerreport
+from .models import Passenger, Worker, ContactUs, Workerreport
 
 
 class WorkerUserForm(forms.ModelForm):
